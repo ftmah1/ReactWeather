1ae9acf859a49631618ee05140dc8a353195ece2
tanwir11 <tanwir11@bitbucket.org>
2017-08-24 04:42:05 +1000

    Prep repo for Heroku

--- server.js
+++ server.js
@@ -1,9 +1,21 @@
 var express = require('express');
 
 var app = express();
+const PORT = process.env.PORT || 3000;
+
+
+app.use(function(req, res, next){
+
+  if(req.headers['x-forwarded-proto']=== 'http'){
+    next();
+  }else{
+    res.redirect('http://' + req.hostname + req.url);
+  }
+
+});
 
 app.use(express.static('public'));  //helps to add functionality to the express module
 
-app.listen(3000, function() {
-  console.log("Server on port 3000");  
+app.listen(port, function() {
+  console.log("Server on port " + PORT);
 });
